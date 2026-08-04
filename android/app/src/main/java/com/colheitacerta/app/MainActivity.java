57f95cfd6486c192227947e0259c8f5df2f5da1f
keypsph <2.58647079e+08+keypsph@users.noreply.github.com>
2026-08-04 00:16:40 +0000

    Commit current state to main

---
+++ android/app/src/main/java/com/colheitacerta/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.colheitacerta.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.colheitacerta.app',
+  appName: 'Colheita Certa',
+  webDir: 'dist'
+};
+
+export default config;
